6c3a5a67b3e8b56e04657f1731577f2e84e485c4
Shiv3006 <116787360+Shiv3006@users.noreply.github.com>
2022-11-02 18:29:29 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -7,11 +7,12 @@ import java.util.List;
 import java.util.Map;
 
 public class Test {
+public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		// System.out.println(getStudentData());
 		List<Student> studentList = getStudentData();
 		Map<String, List<Student>> mapStudent = getMapOfStudent(studentList);
+		System.out.println("-------------------------------------------------");
+		Map<String, List<Student>> mapStudent1 = SortingList(studentList);
 	}
 
 	private static List<Student> getStudentData() {
@@ -27,45 +28,60 @@ public class Test {
 		Subject marathi2 = new Subject("marathi", 77, 100);
 		Subject hindi2 = new Subject("hindi", 88, 100);
 		Subject math3 = new Subject("math", 35, 100);
-		Subject marathi3 = new Subject("marathi", 66, 100);
-		Subject hindi3 = new Subject("hindi", 87, 100);
-		Student studentRitesh = new Student(1, "Ritesh", math1, marathi1, hindi1);
-		Student studentPrateek = new Student(2, "Prateek", math, marathi, hindi);
+		Subject marathi3 = new Subject("marathi", 33, 100);
+		Subject hindi3 = new Subject("hindi", 33, 100);
+		Subject math4 = new Subject("math", 11, 100);
+		Subject marathi4 = new Subject("marathi", 11, 100);
+		Subject hindi4 = new Subject("hindi", 11, 100);
+
+		Student studentPrateek = new Student(1, "Prateek", math, marathi, hindi);
+		Student studentRitesh = new Student(2, "Ritesh", math1, marathi1, hindi1);
 		Student studentShiv = new Student(3, "Shiv", math2, marathi2, hindi2);
 		Student studentSwara = new Student(4, "Swara", math3, marathi3, hindi3);
+		Student studentApurwa = new Student(5, "Apurwa", math4, marathi4, hindi4);
 
 		studentList.add(studentPrateek);
 		studentList.add(studentRitesh);
 		studentList.add(studentShiv);
 		studentList.add(studentSwara);
+		studentList.add(studentApurwa);
 		return studentList;
 	}
 
 	static int greaterThan50Student = 0;
 	static int lessthan50Student = 0;
+	static int studentCnt = 0;
+	static int subjectCnt = 0;
+	static int outOfMarks = 0;
+	static int totalSubjectmarks;
+	static double percentage;
+	static double totalmark;
 
 	private static Map<String, List<Student>> getMapOfStudent(List<Student> studentList) {
+
 		Map<String, List<Student>> studentMap = new HashMap<String, List<Student>>();
 		Map<String, List<Student>> differenceOfMaraksStudents = new HashMap<String, List<Student>>();
 		List<Student> greaterThan50Percentage = new ArrayList<Student>();
 		List<Student> lessThan50Percentage = new ArrayList<Student>();
-		float totalmark;
-		double percentage;
 		for (Student student : studentList) {
 
 			// write percentage logic here and split into studentlistLessThen50percent &
 			// studentlistGreaterThen50percent
-
-			Subject hindi = student.getHindi();
-			Subject marathi = student.getMarathi();
 			Subject math = student.getMath();
+			Subject marathi = student.getMarathi();
+			Subject hindi = student.getHindi();
 
-			totalmark = hindi.getMark() + marathi.getMark() + math.getMark();
-			percentage = ((totalmark / 300) * 100);
+			totalmark = math.getMark() + marathi.getMark() + hindi.getMark();
+			outOfMarks = hindi.getTotalMark() + marathi.getTotalMark() + math.getTotalMark();
+			subjectCnt = outOfMarks / 100;
+			percentage = ((totalmark / outOfMarks) * 100);
 			percentage = Double.parseDouble(new DecimalFormat("##.##").format(percentage));
+			studentCnt++;
+			totalSubjectmarks = 100 * subjectCnt * studentCnt;
 
 			if (percentage > 50) {
 				greaterThan50Percentage.add(student);
+
 				greaterThan50Student++;
 			} else {
 				lessThan50Percentage.add(student);
@@ -81,18 +97,63 @@ public class Test {
 			System.out.print(" math mark : " + math.getMark());
 			System.out.println();
 
-			System.out.println("Total marks of:" + student.getName() + " " + totalmark);
-			System.out.println("Percentage of " + student.getName() + ":" + "" + percentage + "%");
+			System.out.println("Total marks obtained by " + student.getName() + " is: " + totalmark);
+			System.out.println("Total Percentage Obtained by " + student.getName() + " is: " + "" + percentage + "%");
 			System.out.println();
 
 			System.out.println();
 			System.out.println("---------------------------------------------");
+
 		}
-		System.out.println(differenceOfMaraksStudents);
-		System.out.println("count of graeter than 50 percent student :" + greaterThan50Student);
-		System.out.println("count of less than 50 percent student :" + lessthan50Student);
+
+		System.out.println("Total number of students: " + studentCnt);
+		System.out.println("Total number of subjects: " + subjectCnt);
+		System.out.println("Print total subject marks of all students: " + totalSubjectmarks);
+		System.out.println("Out of total marks: " + outOfMarks);
+
+	//	System.out.println(differenceOfMaraksStudents);
+		System.out.println("---------------------------------------------");
+		System.out.println("count of graeter than 50 percent student: " + greaterThan50Student);
+		System.out.println("count of less than 50 percent student: " + lessthan50Student);
 
 		return studentMap;
 	}
+
+	private static Map<String, List<Student>> SortingList(List<Student> studentList) {
+		Map<String, List<Student>> differenceOfMaraksStudents11 = new HashMap<String, List<Student>>();
+		List<Student> greaterThan50Percentage1 = new ArrayList<Student>();
+		List<Student> lessThan50Percentage1 = new ArrayList<Student>();
+		Collections.sort(studentList, new StudentSortByName());
+		for (Student student : studentList) {
+
+			Subject math = student.getMath();
+			Subject marathi = student.getMarathi();
+			Subject hindi = student.getHindi();
+
+			totalmark = math.getMark() + marathi.getMark() + hindi.getMark();
+			outOfMarks = hindi.getTotalMark() + marathi.getTotalMark() + math.getTotalMark();
+			subjectCnt = outOfMarks / 100;
+			percentage = ((totalmark / outOfMarks) * 100);
+			percentage = Double.parseDouble(new DecimalFormat("##.##").format(percentage));
+
+			if (percentage > 50) {
+				greaterThan50Percentage1.add(student);
+				greaterThan50Student++;
+			} else {
+				lessThan50Percentage1.add(student);
+				lessthan50Student++;
+			}
+
+			differenceOfMaraksStudents11.put("name of list grater than 50 per", greaterThan50Percentage1);
+			differenceOfMaraksStudents11.put("name of list less than 50 per", lessThan50Percentage1);
+
+		}
+		System.out.println(differenceOfMaraksStudents11);
+
+		return differenceOfMaraksStudents11;
+
+	}
+
+	
 
 }
