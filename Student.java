8c6d0a4c58330eab77625802d49fbc47c000864c
Shiv3006 <116787360+Shiv3006@users.noreply.github.com>
2022-11-07 19:06:37 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,78 +1,63 @@
-package Com.bnt;
+package com.bnt.model;
 
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Student {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int studId;
 	private int rollNo;
 	private String name;
-	private Subject math;
-	private Subject marathi;
-	private Subject hindi;
-
-	public Student(int rollNo, String name, Subject math, Subject marathi, Subject hindi) {
-		super();
-		this.rollNo = rollNo;
-		this.name = name;
-		this.math = math;
-		this.marathi = marathi;
-		this.hindi = hindi;
+	@OneToMany(mappedBy = "student")
+	private List<Subject> subject;
+	public int getStudId() {
+		return studId;
 	}
-
+	public void setStudId(int studId) {
+		this.studId = studId;
+	}
 	public int getRollNo() {
 		return rollNo;
 	}
-
 	public void setRollNo(int rollNo) {
 		this.rollNo = rollNo;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	public Subject getMath() {
-		return math;
+	public List<Subject> getSubject() {
+		return subject;
 	}
-
-	public void setMath(Subject math) {
-		this.math = math;
+	public void setSubject(List<Subject> subject) {
+		this.subject = subject;
 	}
-
-	public Subject getMarathi() {
-		return marathi;
+	public Student(int studId, int rollNo, String name, List<Subject> subject) {
+		super();
+		this.studId = studId;
+		this.rollNo = rollNo;
+		this.name = name;
+		this.subject = subject;
 	}
-
-	public void setMarathi(Subject marathi) {
-		this.marathi = marathi;
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
-
-	public Subject getHindi() {
-		return hindi;
-	}
-
-	public void setHindi(Subject hindi) {
-		this.hindi = hindi;
-	}
-
 	@Override
 	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("Student [rollNo=");
-		builder.append(rollNo);
-		builder.append(", name=");
-		builder.append(name);
-		builder.append(", math=");
-		builder.append(math);
-		builder.append(", marathi=");
-		builder.append(marathi);
-		builder.append(", hindi=");
-		builder.append(hindi);
-		builder.append("]");
-		return builder.toString();
+		return "Student [studId=" + studId + ", rollNo=" + rollNo + ", name=" + name + ", subject=" + subject + "]";
 	}
-
-
+	
+	
+	
 
 }
